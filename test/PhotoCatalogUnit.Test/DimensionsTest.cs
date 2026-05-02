a216909349c1ee788e9f8ae5ffff28c8b01b4832
Demon6590 <dima.chernytin@gmail.com>
2026-05-02 19:08:15 +0300

    Добавил доку и потправил тесты

--- test/PhotoCatalogUnit.Test/DimensionsTest.cs
+++ test/PhotoCatalogUnit.Test/DimensionsTest.cs
@@ -10,29 +10,29 @@ public class DimensionsTest
     [Fact]
     public void Create_ValidWidthAndHeight_ReturnsSuccess()
     {
-        var actual = Dimensions.Create(100,100);
-        
-        var expectation  = Dimensions.Create(100,100);
-        
-        
+        var actual = Dimensions.Create(100, 100);
+
+        var expectation = Dimensions.Create(100, 100);
+
+
         Assert.Equal(expectation, actual);
     }
 
     [Fact]
     public void Create_MinimumValidSizeOne_ReturnsSuccess()
     {
-        var actual = Dimensions.Create(1,1);
-        
-        var expectation  = Dimensions.Create(1,1);
-        
-        
+        var actual = Dimensions.Create(1, 1);
+
+        var expectation = Dimensions.Create(1, 1);
+
+
         Assert.Equal(expectation, actual);
     }
 
     [Fact]
     public void Create_ZeroValues_ReturnsFailure()
     {
-        var actual = Dimensions.Create(0,0);
+        var actual = Dimensions.Create(0, 0);
         Assert.False(actual.IsSuccess);
         Assert.True(actual.IsFailure);
         Assert.Equal(Result<Dimensions>.Failure(DomainErrors.Dimensions.Invalid), actual.Error);
@@ -41,22 +41,18 @@ public class DimensionsTest
     [Fact]
     public void Create_MaxValues_ReturnsFailure()
     {
-        var actual = Dimensions.Create(3841,2161);
+        var actual = Dimensions.Create(3841, 2161);
         Assert.False(actual.IsSuccess);
         Assert.True(actual.IsFailure);
         Assert.Equal(Result<Dimensions>.Failure(DomainErrors.Dimensions.Invalid), actual.Error);
-        
     }
-    
+
+    [Fact]
     public void Create_NegativeValues_ReturnsFailure()
     {
-        var actual = Dimensions.Create(-1,-1);
+        var actual = Dimensions.Create(-1, -1);
         Assert.False(actual.IsSuccess);
         Assert.True(actual.IsFailure);
         Assert.Equal(Result<Dimensions>.Failure(DomainErrors.Dimensions.Invalid), actual.Error);
-        
     }
-    
-    
-    
 }
