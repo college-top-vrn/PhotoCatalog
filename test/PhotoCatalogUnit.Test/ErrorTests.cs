a47ffba64e2afa9009942a89b86d1e5f5fada868
The-Blight <hezer.fil@gmail.com>
2026-05-02 15:14:57 +0300

    Начала делать unit тесты для Error

---
+++ test/PhotoCatalogUnit.Test/ErrorTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit; 
+using PhotoCatalog.Domain.Primitives;
+
+namespace PhotoCatalogUnit.Test;
+
+/// <summary>
+/// Тесты для проверки поведения объекта <see cref="Error"/>.
+/// </summary>
+public class ErrorTests
+{
+    
+}
