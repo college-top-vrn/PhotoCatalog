d77c130c053f39b6ddc0da640f652a7195a26db1
The-Blight <hezer.fil@gmail.com>
2026-05-02 15:32:06 +0300

    Исправил Error

--- src/PhotoCatalog.Domain/Primitives/Error.cs
+++ src/PhotoCatalog.Domain/Primitives/Error.cs
@@ -3,27 +3,17 @@
 /// <summary>
 /// Представляет структуру для стандартизированного описания бизнес-ошибок в домене.
 /// </summary>
-/// <param name="code">
+/// <param name="Code">
 /// Уникальный строковый идентификатор ошибки (например, "Tag.EmptyName"). 
 /// Используется для программной проверки типа ошибки на прикладном слое.
 /// </param>
-/// <param name="message">
+/// <param name="Message">
 /// Понятное описание проблемы на естественном языке. 
 /// В первую очередь предназначено для логирования и помощи разработчикам при отладке.
 /// </param>
-public readonly struct Error(string code, string message)
+public readonly record struct Error(string Code, string Message)
 {
-    /// <summary>
-    /// Свойство обозначающие код ошибки 
-    /// </summary>
-    public readonly string Code { get; } = code;
-
-    /// <summary>
-    /// Свойство обзначающие человекочитаемое сообщение об ошибки 
-    /// </summary>
-    public readonly string Message { get; } = message;
-
-
+    
     /// <summary>
     /// Специальный объект, обозначающий отсутствие ошибки.
     /// </summary>
