f6a51977681cd9fc846dedff8dc2ccad051cba21
Demon6590 <dima.chernytin@gmail.com>
2026-05-02 18:47:01 +0300

    Добавление Value Object Dimensions

---
+++ src/PhotoCatalog.Domain/ValueObjects/Dimensions.cs
@@ -0,0 +1,30 @@
+﻿using PhotoCatalog.Domain.Primitives;
+
+namespace PhotoCatalog.Domain.ValueObjects;
+
+
+
+public record Dimensions
+{
+    private const int MaxWidth = 3840;
+    private const int MaxHeight = 2160;
+    private const int MinValues = 1;
+    public int Width { get; init; }
+    public int Height { get; init; }
+
+    private Dimensions(int width, int height)
+    {   
+        Width = width;
+        Height = height;
+    }
+
+    public static Result<Dimensions> Create(int width, int height)
+    {
+        if ((width >= MinValues && width <= MaxWidth) || (height >= MinValues  && height <= MaxHeight))
+        {
+            return Result<Dimensions>.Success(new Dimensions(width, height));
+        }
+
+        return Result<Dimensions>.Failure(new Error("Dimensions.Invalid", "Ширина и высота должны быть больше нуля")); // TODO Добавить ошибку при отрицательной длины ширены
+    }
+};
---
+++ test/PhotoCatalogUnit.Test/DimensionsTest.cs
@@ -0,0 +1,51 @@
+﻿using PhotoCatalog.Domain.Primitives;
+using PhotoCatalog.Domain.ValueObjects;
+
+using Xunit;
+
+namespace PhotoCatalogUnit.Test;
+
+public class DimensionsTest
+{
+    [Fact]
+    public void Create_ValidWidthAndHeight_ReturnsSuccess()
+    {
+        var actual = Dimensions.Create(100,100);
+        
+        var expectation  = Dimensions.Create(100,100);
+        
+        
+        Assert.Equal(expectation, actual);
+    }
+
+    [Fact]
+    public void Create_MinimumValidSizeOne_ReturnsSuccess()
+    {
+        var actual = Dimensions.Create(1,1);
+        
+        var expectation  = Dimensions.Create(1,1);
+        
+        
+        Assert.Equal(expectation, actual);
+    }
+
+    [Fact]
+    public void Create_ZeroValues_ReturnsFailure()
+    {
+        var actual = Dimensions.Create(0,0);
+        Assert.False(actual.IsSuccess);
+        Assert.True(actual.IsFailure);
+        Assert.Equal(Result<Dimensions>.Failure(new Error("Dimensions.Invalid", "Ширина и высота должны быть больше нуля")), actual.Error);
+    }
+    
+    public void Create_NegativeValues_ReturnsFailure()
+    {
+        var actual = Dimensions.Create(-1,-1);
+        Assert.False(actual.IsSuccess);
+        Assert.True(actual.IsFailure);
+        Assert.Equal(Result<Dimensions>.Failure(new Error("Dimensions.Invalid", "Ширина и высота должны быть больше нуля")), actual.Error);
+        
+    }
+    
+    
+}
