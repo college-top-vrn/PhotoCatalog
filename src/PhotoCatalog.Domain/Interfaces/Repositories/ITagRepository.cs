5750f410d4af0c43cae27b04712ec5cc6d04d891
danechka <danchik3859@gmail.com>
2026-05-04 19:24:19 +0300

    ITegRepository

---
+++ src/PhotoCatalog.Domain/Interfaces/Repositories/ITagRepository.cs
@@ -0,0 +1,38 @@
+﻿using PhotoCatalog.Domain.Entities;
+using PhotoCatalog.Domain.Primitives;
+
+namespace PhotoCatalog.Domain.Interfaces.Repositories;
+
+/// <summary>
+/// Репозиторий для работы с тегами.
+/// </summary>
+public interface ITagRepository
+{
+    /// <summary>
+    /// Получает тег по ID.
+    /// </summary>
+    /// <param name="id">ID тега</param>
+    /// <returns>Тег или ошибка</returns>
+    Result<Tag> GetById(int id);
+
+    /// <summary>
+    /// Получает тег по имени.
+    /// </summary>
+    /// <param name="name">Имя тега</param>
+    /// <returns>Тег или ошибка</returns>
+    Result<Tag> GetByName(string name);
+
+    /// <summary>
+    /// Добавляет тег.
+    /// </summary>
+    /// <param name="tag">Тег для добавления</param>
+    /// <returns>Результат операции</returns>
+    ResultVoid Add(Tag tag);
+
+    /// <summary>
+    /// Удаляет тег по ID.
+    /// </summary>
+    /// <param name="id">ID тега</param>
+    /// <returns>Удаленный тег или ошибка</returns>
+    Result<Tag> Delete(int id);
+}
