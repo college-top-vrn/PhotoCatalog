3aa21c4b011b21369d30104707b3490af86f6262
Semyon <normal.man101@outlook.com>
2026-05-05 21:11:51 +0300

    Написал все тесты по заданию

--- test/PhotoCatalog.Test.Architectural/ArchitectureProvider.cs
+++ test/PhotoCatalog.Test.Architectural/ArchitectureProvider.cs
@@ -1,58 +1,55 @@
 using ArchUnitNET.Domain;
-using ArchUnitNET.Fluent;
 using ArchUnitNET.Loader;
-using ArchUnitNET;
-
-using System;
-
-using ArchUnitNET.Fluent.Syntax.Elements.Types.Classes;
+using ArchUnitNET.xUnit;
 
 using PhotoCatalog.Application.Errors;
 using PhotoCatalog.Domain.Entities;
-using PhotoCatalog.Domain.Extensions;
-using PhotoCatalog.Domain.Interfaces.Repositories;
-using PhotoCatalog.Domain.Interfaces.Services;
-using PhotoCatalog.Domain.Primitives;
-using PhotoCatalog.Domain.ValueObjects;
+
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
 
 using Xunit;
 
 namespace PhotoCatalog.Test.Architectural;
 
-using ArchUnitNET;
-
 public static class ArchitectureProvider
 {
-    public static readonly ArchUnitNET.Domain.Architecture ApplicationLayer =
+    public static readonly Architecture Architecture =
         new ArchLoader()
             .LoadAssemblies(
-                typeof(ApplicationErrors).Assembly)
-            .Build();
+                typeof(ApplicationErrors).Assembly,
+                typeof(Album).Assembly
+            ).Build();
 
-    public static readonly ArchUnitNET.Domain.Architecture DomainLayer =
-        new ArchLoader()
-            .LoadAssemblies(typeof(Album).Assembly,
-                typeof(Folder).Assembly,
-                typeof(Photo).Assembly,
-                typeof(Tag).Assembly,
-                typeof(ResultExtensions).Assembly,
-                typeof(IAlbumRepository).Assembly,
-                typeof(IFolderRepository).Assembly,
-                typeof(IPhotoRepository).Assembly,
-                typeof(ITagRepository).Assembly,
-                typeof(IFileMetadataExtractor).Assembly,
-                typeof(IFileStorage).Assembly,
-                typeof(DomainErrors).Assembly,
-                typeof(Error).Assembly,
-                typeof(Result<>).Assembly,
-                typeof(ResultVoid).Assembly,
-                typeof(SystemErrors).Assembly,
-                typeof(Dimensions).Assembly
-                )
-            .Build();
-    
+    public static readonly IObjectProvider<IType> ApplicationLayer =
+        Types().That().ResideInNamespaceMatching("PhotoCatalog.Application.*");
+
+    public static readonly IObjectProvider<IType> DomainLayer =
+        Types().That().ResideInNamespaceMatching("PhotoCatalog.Domain.*");
+
+    public static readonly IObjectProvider<IType> InfrastructureLayer =
+        Types().That().ResideInNamespaceMatching("PhotoCatalog.Infrastructure.*");
+
     [Fact]
     public static void Domain_ShouldNot_HaveDependencies_OnOtherLayers()
     {
+        Classes().That().Are(DomainLayer).Should().NotDependOnAny(ApplicationLayer).Check(Architecture);
+    }
+
+    [Fact]
+    public static void Application_ShouldNot_HaveDependencies_OnInfrastructure()
+    {
+        Classes().That().Are(ApplicationLayer).Should().NotDependOnAny(InfrastructureLayer).Check(Architecture);
+    }
+
+    [Fact]
+    public static void Infrastructure_Should_DependOn_Domain()
+    {
+        Classes().That().Are(InfrastructureLayer).Should().DependOnAny(DomainLayer).Check(Architecture);
+    }
+
+    [Fact]
+    public static void DomainClasses_Should_BeInNamespace_That_StartsWith_PhotoCatalogDomain()
+    {
+        Classes().That().Are(DomainLayer).Should().ResideInNamespaceMatching(@"PhotoCatalog.Domain.*");
     }
 }
