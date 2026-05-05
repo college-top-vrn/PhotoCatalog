f135dbc084e52840515a347ec757f6b06d42dedb
Demon6590 <dima.chernytin@gmail.com>
2026-05-05 12:02:52 +0300

    добавил AddPhotoToAlbumUseCase

---
+++ src/PhotoCatalog.Application/UseCases/AddPhotoToAlbumUseCase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+using PhotoCatalog.Application.Errors;
+using PhotoCatalog.Domain.Entities;
+using PhotoCatalog.Domain.Interfaces.Repositories;
+using PhotoCatalog.Domain.Interfaces.Services;
+using PhotoCatalog.Domain.Primitives;
+
+using Serilog;
+using Serilog.Events;
+
+namespace PhotoCatalog.Application.UseCases;
+
+public class AddPhotoToAlbumUseCase
+{
+    private IAlbumRepository _albumRepository;
+    private IPhotoRepository _photoRepository;
+    private IUnitOfWork _unitOfWork;
+    private ILogger _logger;
+
+    public AddPhotoToAlbumUseCase(IAlbumRepository albumRepository, IPhotoRepository photoRepository,IUnitOfWork unitOfWork, ILogger logger)
+    {
+        _albumRepository = albumRepository;
+        _photoRepository = photoRepository;
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    
+    public ResultVoid Execute(int albumId, int photoId)
+    {
+        var photo = _photoRepository.GetById(photoId);
+        
+        if (photo.IsFailure)
+            return ResultVoid.Failure(ApplicationErrors.General.NotFound);
+        
+        var album = _albumRepository.GetById(albumId);
+        
+        if (album.IsFailure)
+            return ResultVoid.Failure(ApplicationErrors.General.NotFound);
+
+        var addResult = album.Value.AddPhoto(photoId);
+        
+        if (addResult.IsFailure)
+            return ResultVoid.Failure(addResult.Error);
+        
+        
+    }
+}
