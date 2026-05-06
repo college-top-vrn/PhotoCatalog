0e6efa36662529a2d8f12a2ac378f2ae52dd6d50
Semyon <normal.man101@outlook.com>
2026-05-06 22:14:08 +0300

    Начал реализацию ТЗ

--- src/PhotoCatalog.Application/UseCases/DeletePhotoUseCase.cs
+++ src/PhotoCatalog.Application/UseCases/DeletePhotoUseCase.cs
@@ -1,5 +1,30 @@
+using PhotoCatalog.Domain.Interfaces.Repositories;
+using PhotoCatalog.Domain.Interfaces.Services;
+
+using Microsoft.Extensions.Logging;
+
+using PhotoCatalog.Application.Errors;
+using PhotoCatalog.Domain.Extensions;
+using PhotoCatalog.Domain.Primitives;
+
+using Serilog;
+
 namespace PhotoCatalog.Application.UseCases;
 
-public class DeletePhotoUseCase
+public class DeletePhotoUseCase(IPhotoRepository photoRepository, IFileStorage fileStorage, IUnitOfWork unitOfWork, ILogger<DeletePhotoUseCase> logger)
 {
+    private readonly IPhotoRepository _photoRepository = photoRepository;
+    private readonly IFileStorage _fileStorage = fileStorage;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly ILogger<DeletePhotoUseCase> _logger = logger;
+
+    public ResultVoid Execute(int photoId)
+    {
+        return _photoRepository
+            .GetById(photoId)
+            .ToResult(ApplicationErrors.General.NotFound)
+            .Then(photo => _unitOfWork.BeginTransaction())
+            .Then(() => _photoRepository.Delete(photoId))
+            .Then(() => _unitOfWork.Commit()).ToResult(new Error("Тест", "Тест"));
+    }
 }
