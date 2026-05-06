e6cdd724b9981b3110e06febe2763b46e3435864
Demon6590 <dima.chernytin@gmail.com>
2026-05-06 10:33:19 +0300

    добавил AddPhotoToAlbumUseCase

---
+++ src/PhotoCatalog.Application/UseCases/AddPhotoToAlbumUseCase.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+using PhotoCatalog.Application.Errors;
+using PhotoCatalog.Domain.Interfaces.Repositories;
+using PhotoCatalog.Domain.Interfaces.Services;
+using PhotoCatalog.Domain.Primitives;
+
+
+namespace PhotoCatalog.Application.UseCases;
+
+/// <summary>
+/// Сценарий использования для добавления фотографии в существующий альбом.
+/// </summary>
+public class AddPhotoToAlbumUseCase
+{
+    private readonly IAlbumRepository _albumRepository;
+    private readonly IPhotoRepository _photoRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger _logger;
+
+    public AddPhotoToAlbumUseCase(IAlbumRepository albumRepository, IPhotoRepository photoRepository,
+        IUnitOfWork unitOfWork, ILogger logger)
+    {
+        _albumRepository = albumRepository;
+        _photoRepository = photoRepository;
+        _unitOfWork = unitOfWork;
+        _logger = logger.ForContext<AddPhotoToAlbumUseCase>();
+    }
+
+    /// <summary>
+    /// Выполняет добавление фотографии в альбом с сохранением изменений в рамках транзакции.
+    /// </summary>
+    /// <param name="albumId">Идентификатор альбома.</param>
+    /// <param name="photoId">Идентификатор фотографии.</param>
+    /// <returns>Результат выполнения операции (успех или ошибка).</returns>
+    public ResultVoid Execute(int albumId, int photoId)
+    {
+        _logger.Information("Запуск процесса добавления фото {PhotoId} в альбом {AlbumId}", photoId, albumId);
+        var photo = _photoRepository.GetById(photoId);
+
+        if (photo.IsFailure)
+        {
+            _logger.Warning("Фото {PhotoId} не найдено", photoId);
+            return ResultVoid.Failure(ApplicationErrors.General.NotFound);
+        }
+
+        var album = _albumRepository.GetById(albumId);
+
+        if (album.IsFailure)
+        {
+            _logger.Warning("Альбом {AlbumId} не найден", albumId);
+            return ResultVoid.Failure(ApplicationErrors.General.NotFound);
+        }
+
+        var addResult = album.Value!.AddPhoto(photoId);
+
+        if (addResult.IsFailure)
+        {
+            _logger.Warning("Не удалось добавить фото {PhotoId} в альбом {AlbumId}: {Error}", photoId, albumId,
+                addResult.Error);
+            return
+                ResultVoid.Failure(addResult
+                    .Error); // TODO сделать ошибку для album в ApplicationErrors если есть дубликат.
+        }
+
+        var beginResult = _unitOfWork.BeginTransaction();
+        if (beginResult.IsFailure)
+            return ResultVoid.Failure(beginResult.Error); // TODO сделать ошибку транзакции в ApplicationErrors.
+
+        try
+        {
+            var updateResult = _albumRepository.Update(album.Value);
+            if (updateResult.IsFailure)
+            {
+                _unitOfWork.Rollback();
+                return ResultVoid.Failure(updateResult.Error); // TODO сделать ошибку транзакции в ApplicationErrors.
+            }
+
+            var commitResult = _unitOfWork.Commit();
+            if (commitResult.IsSuccess)
+            {
+                _logger.Information("Фото {PhotoId} успешно добавлено в альбом {AlbumId}", photoId, albumId);
+                return ResultVoid.Success();
+            }
+
+            _unitOfWork.Rollback();
+            return ResultVoid.Failure(commitResult.Error); // TODO сделать ошибку транзакции в ApplicationErrors.
+        }
+        catch (Exception ex)
+        {
+            _unitOfWork.Rollback();
+            _logger.Error(ex, "Критическая ошибка при сохранении изменений для альбома {AlbumId}", albumId);
+            return ResultVoid.Failure(default); // TODO сделать системную ошибку в ApplicationErrors.
+        }
+    }
+}
