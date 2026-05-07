a2c7efb076d9611f4261e8bf3531d4b17bd804bf
emodkid <ivan.galkin087@gmail.com>
2026-05-07 12:59:17 +0300

    начало реализации

--- src/PhotoCatalog.Application/UseCases/ImportPhotoUseCase.cs
+++ src/PhotoCatalog.Application/UseCases/ImportPhotoUseCase.cs
@@ -1,6 +1,19 @@
-namespace PhotoCatalog.Application;
+using Microsoft.Extensions.Logging;
 
-public class ImportPhotoUseCase
+using PhotoCatalog.Domain.Interfaces.Repositories;
+using PhotoCatalog.Domain.Interfaces.Services;
+using PhotoCatalog.Domain.Primitives;
+
+namespace PhotoCatalog.Application.UseCases;
+
+public class ImportPhotoUseCase(
+    IPhotoRepository photoRepository,
+    IFileStorage fileStorage,
+    IUnitOfWork unitOfWork,
+    ILogger<ImportPhotoUseCase> logger)
 {
-    
+    public Result<PhotoResponse> Execute(ImportPhotoRequest request)
+    {
+        
+    }
 }
