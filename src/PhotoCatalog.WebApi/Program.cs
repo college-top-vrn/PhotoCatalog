cbe26bf7a9566e284cfa035f096b81c96e57058a
The-Blight <hezer.fil@gmail.com>
2026-05-12 16:50:01 +0300

    Убрал implicit using в слое infrastructure и добавил ссылку на инфраструктуры в csproj WebApi

--- src/PhotoCatalog.WebApi/Program.cs
+++ src/PhotoCatalog.WebApi/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using PhotoCatalog.Infrastructure.Repositories;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
