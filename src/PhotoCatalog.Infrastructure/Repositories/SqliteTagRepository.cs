ed8059af6ee11a3289e22b390fd6dec2fa18cc36
Dan1chka <danchik3859@gmail.com>
2026-05-14 09:53:11 +0300

    Сделали репозиторий SqliteFolderRepository

--- src/PhotoCatalog.Infrastructure/Repositories/SqliteTagRepository.cs
+++ src/PhotoCatalog.Infrastructure/Repositories/SqliteTagRepository.cs
@@ -3,7 +3,7 @@ using System;
 using Dapper;
 
 using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 using PhotoCatalog.Domain.Entities;
 using PhotoCatalog.Domain.Interfaces.Repositories;
@@ -23,7 +23,7 @@ namespace PhotoCatalog.Infrastructure.Repositories;
 public class SqliteTagRepository : ITagRepository
 {
     private readonly string _connectionString;
-    private readonly ILogger<SqliteAlbumRepository> _logger;
+    private readonly ILogger _logger;
 
     /// <summary>
     /// Создает репозиторий с указанной строкой подключения к SQLite.
@@ -32,7 +32,7 @@ public class SqliteTagRepository : ITagRepository
     /// Строка подключения вида "Data Source=photo.db".
     /// </param>
     /// <param name="logger">Логгер для записи диагностических сообщений и ошибок.</param>
-    public SqliteTagRepository(string connectionString, ILogger<SqliteAlbumRepository> logger)
+    public SqliteTagRepository(string connectionString, ILogger logger)
     {
         _connectionString = connectionString;
         _logger = logger;
@@ -65,12 +65,12 @@ public class SqliteTagRepository : ITagRepository
         }
         catch (SqliteException ex)
         {
-            _logger.LogError(ex, "Ошибка SQLite в методе GetById для тега {Id}", id);
+            _logger.Error(ex, "Ошибка SQLite в методе GetById для тега {Id}", id);
             return Result<Tag>.Failure(InfrastructureErrors.Database.ConnectionFailed);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Неожиданная ошибка в методе GetById для тега {Id}", id);
+            _logger.Error(ex, "Неожиданная ошибка в методе GetById для тега {Id}", id);
             return Result<Tag>.Failure(InfrastructureErrors.Database.ConnectionFailed);
         }
     }
@@ -102,24 +102,81 @@ public class SqliteTagRepository : ITagRepository
         }
         catch (SqliteException ex)
         {
-            _logger.LogError(ex, "Ошибка SQLite в методе GetByName для тега {Name}", name);
+            _logger.Error(ex, "Ошибка SQLite в методе GetByName для тега {Name}", name);
             return Result<Tag>.Failure(InfrastructureErrors.Database.ConnectionFailed);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Неожиданная ошибка в методе GetByName для тега {Name}", name);
+            _logger.Error(ex, "Неожиданная ошибка в методе GetByName для тега {Name}", name);
             return Result<Tag>.Failure(InfrastructureErrors.Database.ConnectionFailed);
         }
     }
 
 
-    public ResultVoid Add(Tag tag)
+
+
+    /// <inheritdoc />
+    public ResultVoid Add(Tag teg)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using SqliteConnection connection = new(_connectionString);
+            connection.Open();
+            const string sql = """
+                               UPDATE Folders
+                               SET ParentFolderId = @ParentFolderId,
+                                   Name = @Name
+                               WHERE Id = @Id
+                               """;
+
+            int affectedRows = connection.Execute(sql, new {teg.Id, teg.Name });
+
+            if (affectedRows == 0)
+            {
+                _logger.Warning("Не удалось обновить несуществующий тег с Id = {Id}", teg.Id);
+                return ResultVoid.Failure(InfrastructureErrors.Database.NotFound);
+            }
+
+            _logger.Information("Тег с Id = {Id} успешно обновлен",teg.Id);
+            return ResultVoid.Success();
+        }
+        catch (SqliteException ex)
+        {
+            _logger.Error(ex, "Ошибка SQLite при обновлении тега с Id = {Id}", teg.Id);
+            return ResultVoid.Failure(InfrastructureErrors.Database.Sqlite);
+        }
     }
 
+    /// <inheritdoc />
     public ResultVoid Delete(int id)
     {
-        throw new NotImplementedException();
+        const int sqliteConstraintErrorCode = 19;
+        try
+        {
+            using SqliteConnection connection = new(_connectionString);
+            connection.Open();
+
+            const string sql = "DELETE FROM Folders WHERE Id = @Id";
+            int affectedRows = connection.Execute(sql, new { Id = id });
+
+            if (affectedRows == 0)
+            {
+                _logger.Warning("Попытка удалить несуществующий тег с Id = {FolderId}", id);
+                return ResultVoid.Failure(InfrastructureErrors.Database.NotFound);
+            }
+
+            _logger.Information("Тег с Id = {Id} успешно удалена", id);
+            return ResultVoid.Success();
+        }
+        catch (SqliteException ex) when (ex.SqliteErrorCode == sqliteConstraintErrorCode)
+        {
+            _logger.Warning(ex, "Невозможно удалить тег с Id = {Id}: имеются дочерние объекты",id);
+            return ResultVoid.Failure(InfrastructureErrors.Database.HasChildren);
+        }
+        catch (SqliteException ex)
+        {
+            _logger.Error(ex, "Ошибка SQLite при удалении тега с Id = {Id}", id);
+            return ResultVoid.Failure(InfrastructureErrors.Database.Sqlite);
+        }
     }
 }
