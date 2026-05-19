995cf6e91fd5a245675c1f77279ccfea18d37261
Semyon <normal.man101@outlook.com>
2026-05-19 09:58:18 +0300

    Переделал реализацию команд на изменение БД

--- src/PhotoCatalog.Infrastructure/Repositories/SqliteFolderCommandRepository.cs
+++ src/PhotoCatalog.Infrastructure/Repositories/SqliteFolderCommandRepository.cs
@@ -1,105 +1,75 @@
 using Dapper;
 
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
 
 using PhotoCatalog.Domain.Entities;
+using PhotoCatalog.Domain.Extensions;
 using PhotoCatalog.Domain.Interfaces.Repositories;
+using PhotoCatalog.Domain.Interfaces.Services;
 using PhotoCatalog.Domain.Primitives;
 using PhotoCatalog.Infrastructure.Errors;
+using PhotoCatalog.Infrastructure.UnitOfWork;
 
 using Serilog;
 
 namespace PhotoCatalog.Infrastructure.Repositories;
 
 /// <inheritdoc />
-public class SqliteFolderCommandRepository(string connectionString, ILogger logger) : IFolderCommandRepository
+public class SqliteFolderCommandRepository : IFolderCommandRepository
 {
+    SqliteFolderCommandRepository(string connectionString, ILogger<SqliteUnitOfWork> logger)
+    {
+        _unitOfWork = new SqliteUnitOfWork(connectionString, logger);
+        _logger = logger;
+    }
+
+    private readonly SqliteUnitOfWork _unitOfWork;
+    private readonly ILogger<SqliteUnitOfWork> _logger;
+
     /// <inheritdoc />
     public ResultVoid Add(Folder folder)
     {
-        try
-        {
-            using SqliteConnection connection = new(connectionString);
-            connection.Open();
-            const string sql = """
-                               INSERT INTO Folders (Id, ParentFolderId, Name)
-                               VALUES (@Id, @ParentFolderId, @Name)
-                               """;
-            connection.Execute(sql, new { folder.Id, folder.ParentFolderId, folder.Name });
-
-            logger.Information("Папка с Id = {FolderId} успешно добавлена", folder.Id);
-
-            return ResultVoid.Success();
-        }
-        catch (SqliteException ex)
-        {
-            logger.Error(ex, "Ошибка SQLite при добавлении папки с Id = {FolderId}", folder.Id);
-            return ResultVoid.Failure(InfrastructureErrors.Database.Sqlite);
-        }
+        return _unitOfWork.Connection!
+            .Execute(
+                """
+                INSERT INTO Folders (Id, ParentFolderId, Name)
+                VALUES (@Id, @ParentFolderId, @Name)
+                """,
+                new { folder.Id, folder.ParentFolderId, folder.Name })
+            .ToResult()
+            .OnSuccess(_ => _unitOfWork.Commit())
+            .OnFailure(_ => _unitOfWork.Rollback());
     }
 
     /// <inheritdoc />
     public ResultVoid Update(Folder folder)
     {
-        try
-        {
-            using SqliteConnection connection = new(connectionString);
-            connection.Open();
-            const string sql = """
-                               UPDATE Folders
-                               SET ParentFolderId = @ParentFolderId,
-                                   Name = @Name
-                               WHERE Id = @Id
-                               """;
-
-            int affectedRows = connection.Execute(sql, new { folder.Id, folder.ParentFolderId, folder.Name });
-
-            if (affectedRows == 0)
-            {
-                logger.Warning("Не удалось обновить несуществующую папку с Id = {FolderId}", folder.Id);
-                return ResultVoid.Failure(InfrastructureErrors.Database.NotFound);
-            }
-
-            logger.Information("Папка с Id = {FolderId} успешно обновлена", folder.Id);
-            return ResultVoid.Success();
-        }
-        catch (SqliteException ex)
-        {
-            logger.Error(ex, "Ошибка SQLite при обновлении папки с Id = {FolderId}", folder.Id);
-            return ResultVoid.Failure(InfrastructureErrors.Database.Sqlite);
-        }
+        return _unitOfWork.Connection!
+            .Execute(
+                """
+                UPDATE Folders
+                SET ParentFolderId = @ParentFolderId,
+                Name = @Name
+                WHERE Id = @Id
+                """,
+                new { folder.Id, folder.ParentFolderId, folder.Name }
+            )
+            .ToResult()
+            .Check(affectedRows => (affectedRows != 0).ToResult())
+            .OnSuccess(_ => _unitOfWork.Commit())
+            .OnFailure(_ => _unitOfWork.Rollback());
     }
 
     /// <inheritdoc />
     public ResultVoid Delete(int id)
     {
-        const int sqliteConstraintErrorCode = 19;
-        try
-        {
-            using SqliteConnection connection = new(connectionString);
-            connection.Open();
-
-            const string sql = "DELETE FROM Folders WHERE Id = @Id";
-            int affectedRows = connection.Execute(sql, new { Id = id });
-
-            if (affectedRows == 0)
-            {
-                logger.Warning("Попытка удалить несуществующую папку с Id = {FolderId}", id);
-                return ResultVoid.Failure(InfrastructureErrors.Database.NotFound);
-            }
-
-            logger.Information("Папка с Id = {FolderId} успешно удалена", id);
-            return ResultVoid.Success();
-        }
-        catch (SqliteException ex) when (ex.SqliteErrorCode == sqliteConstraintErrorCode)
-        {
-            logger.Warning(ex, "Невозможно удалить папку с Id = {FolderId}: имеются дочерние объекты", id);
-            return ResultVoid.Failure(InfrastructureErrors.Database.HasChildren);
-        }
-        catch (SqliteException ex)
-        {
-            logger.Error(ex, "Ошибка SQLite при удалении папки с Id = {FolderId}", id);
-            return ResultVoid.Failure(InfrastructureErrors.Database.Sqlite);
-        }
+        return _unitOfWork.Connection!
+            .Execute("DELETE FROM Folders WHERE Id = @Id",
+                new { Id = id })
+            .ToResult()
+            .Check(affectedRows => (affectedRows != 0).ToResult())
+            .OnSuccess(_ => _unitOfWork.Commit())
+            .OnFailure(_ => _unitOfWork.Rollback());
     }
 }
