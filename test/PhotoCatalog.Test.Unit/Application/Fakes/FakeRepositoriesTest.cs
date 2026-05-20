f69cd9cdf819d302af889c87fd9b5c03f5c154de
emodkid <ivan.galkin087@gmail.com>
2026-05-20 13:32:11 +0300

    добавил xml документацию

--- test/PhotoCatalog.Test.Unit/Application/Fakes/FakeRepositoriesTest.cs
+++ test/PhotoCatalog.Test.Unit/Application/Fakes/FakeRepositoriesTest.cs
@@ -4,10 +4,16 @@ using PhotoCatalog.Infrastructure.Fakes;
 
 using Xunit;
 
-namespace PhotoCatalog.Test.Unit;
+namespace PhotoCatalog.Test.Unit.Application.Fakes;
 
+/// <summary>
+///     Содержит модульные тесты для проверки работы FakeAlbumRepository.
+/// </summary>
 public static class FakeRepositoriesTest
 {
+    /// <summary>
+    ///     Проверяет, что альбом успешно добавляется в репозиторий при передаче корректных значений.
+    /// </summary>
     [Fact]
     public static void AlbumRepository_AddAlbum_WithRightValues()
     {
@@ -20,6 +26,9 @@ public static class FakeRepositoriesTest
         Assert.Equal("Test", albumName);
     }
 
+    /// <summary>
+    ///     Проверяет, что при попытке добавить null возвращается ожидаемая ошибка.
+    /// </summary>
     [Fact]
     public static void AlbumRepository_AddAlbum_AddingNull()
     {
@@ -34,6 +43,9 @@ public static class FakeRepositoriesTest
         Assert.Equal(result.Message, expectedResult.Message);
     }
 
+    /// <summary>
+    ///     Проверяет, что альбом успешно обновляется при передаче корректных значений.
+    /// </summary>
     [Fact]
     public static void AlbumRepository_UpdateAlbum_WithRightValues()
     {
@@ -50,6 +62,9 @@ public static class FakeRepositoriesTest
         Assert.NotEqual(oldAlbum.Value.Name, newAlbum.Value.Name);
     }
 
+    /// <summary>
+    ///     Проверяет, что при попытке обновить альбом с несуществующим идентификатором возвращается ошибка.
+    /// </summary>
     [Fact]
     public static void AlbumRepository_UpdateAlbum_UpdatingWithNonexistentId()
     {
@@ -64,6 +79,9 @@ public static class FakeRepositoriesTest
         Assert.Equal(resultError.Message, expectedError.Message);
     }
 
+    /// <summary>
+    ///     Проверяет, что при попытке обновить null возвращается ожидаемая ошибка.
+    /// </summary>
     [Fact]
     public static void AlbumRepository_UpdateAlbum_UpdatingWithNull()
     {
@@ -78,6 +96,9 @@ public static class FakeRepositoriesTest
         Assert.Equal(resultError.Message, expectedError.Message);
     }
 
+    /// <summary>
+    ///     Проверяет, что существующий альбом успешно удаляется.
+    /// </summary>
     [Fact]
     public static void AlbumRepository_DeleteAlbum_WithExistingId()
     {
@@ -103,6 +124,9 @@ public static class FakeRepositoriesTest
         Assert.Equal(expectedError.Message, searchResult.Error.Message);
     }
 
+    /// <summary>
+    ///     Проверяет, что при попытке удалить альбом с несуществующим идентификатором возвращается ошибка.
+    /// </summary>
     [Fact]
     public static void AlbumRepository_DeleteAlbum_WithNonexistentId()
     {
