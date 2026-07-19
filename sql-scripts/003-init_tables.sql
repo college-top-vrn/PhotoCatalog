d249ab98e5c16caca576f82475c79d65a4fcd2d3
Semyon <normal.man101@outlook.com>
2026-07-19 21:57:04 +0300

    feat: Добавил колонку для хранения размера и типа файла

--- sql-scripts/003-init_tables.sql
+++ sql-scripts/003-init_tables.sql
@@ -1,12 +1,13 @@
 CREATE TABLE photos
 (
-    id        UUID PRIMARY KEY DEFAULT uuidv7(),
-    --Написать нужные для бизнеса метаданные,
-    user_id   UUID  NOT NULL,
-    photo_key TEXT  NOT NULL,
-    metadata  JSONB NOT NULL   DEFAULT '{}'::jsonb,
+    id          UUID PRIMARY KEY DEFAULT uuidv7(),
+    user_id     UUID    NOT NULL,
+    photo_size  INTEGER NOT NULL,
+    mime        TEXT    NOT NULL,
+    storage_key TEXT    NOT NULL,
+    metadata    JSONB   NOT NULL DEFAULT '{}'::jsonb,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE RESTRICT,
-    CONSTRAINT unq_photo_key UNIQUE (photo_key)
+    CONSTRAINT unq_storage_key UNIQUE (storage_key)
 );
 
-CREATE INDEX idx_metadata ON photos USING gin (metadata)
+CREATE INDEX idx_metadata ON photos USING gin (metadata);
